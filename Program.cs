c32676a187c8697cf3d8e0018ddf25992795ecc4
Zo PHPCoder99 <gergenkopzoya@gmail.com>
2023-03-21 11:17:43 +0700

    added readarray and writearray functions, minor changes to readme

--- Program.cs
+++ Program.cs
@@ -4,6 +4,16 @@ namespace gbkontrolnaya
 {
     class Program
     {
+        string[] ReadArray(){
+            return Console.ReadLine().Split();
+        }
+
+        void WriteArray(string[] array){
+            for(int i = 0; i < array.Length; i++){
+                Console.Write(array[i] + " ");
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");

eb413d96dfcc01c8de6766097523e12d1f781888
Zo PHPCoder99 <gergenkopzoya@gmail.com>
2023-03-21 11:21:16 +0700

    added lengthlessthanorequalto3 function, error fixes (added static before functions,) changed delimiter to ', ' in writearray

--- Program.cs
+++ Program.cs
@@ -4,19 +4,33 @@ namespace gbkontrolnaya
 {
     class Program
     {
-        string[] ReadArray(){
+        static string[] ReadArray(){
             return Console.ReadLine().Split();
         }
 
-        void WriteArray(string[] array){
+        static string[] LengthLessThanOrEqualTo3(string[] array){
+            string[] result = new string[array.Length];
+            int j = 0;
             for(int i = 0; i < array.Length; i++){
-                Console.Write(array[i] + " ");
+                if (array[i].Length <= 3)
+                {
+                    result[j] = array[i];
+                    j++;
+                }
+            }
+            return result;
+        }
+
+        static void WriteArray(string[] array){
+            for(int i = 0; i < array.Length; i++){
+                Console.Write(array[i] + ", ");
             }
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter an array: ");
+            WriteArray(LengthLessThanOrEqualTo3(ReadArray()));
         }
     }
 }
